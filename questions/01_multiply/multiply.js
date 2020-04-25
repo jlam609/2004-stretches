1c1a689d99cee291a892ad0d7a9aa76b68010ea0
jlam609 <jlam609@gmail.com>
2020-04-25 09:17:39 -0400

    finished multiply

--- questions/01_multiply/multiply.js
+++ questions/01_multiply/multiply.js
@@ -1,5 +1,7 @@
 // This stretch requires you to write your own test cases
 
-const multiply = (num1, num2) => {};
-
+const multiply = (num1, num2) => {
+if (num1 === undefined || num2 === undefined) throw 'must give 2 numbers'
+return num1 * num2
+}
 module.exports = { multiply };
