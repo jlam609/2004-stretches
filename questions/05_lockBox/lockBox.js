ac9482783163186a5a64ad1a74c25843de4d1991
jlam609 <jlam609@gmail.com>
2020-05-04 19:03:48 -0400

    finished

--- questions/05_lockBox/lockBox.js
+++ questions/05_lockBox/lockBox.js
@@ -1,23 +1,23 @@
 const lockBox = (code, message) => {
   // write code here
   this.prototype = lockBox;
-  if (typeof message !== 'string') throw 'error';
-
-  const revealMessage = (newCode2) => {
-    if (newCode === code) return message;
-    if (code !== newCode2) throw 'error';
+  if (typeof message !== 'string' || typeof code !== 'number') throw 'invalid type';
+  let accessCode = code;
+  let secretMessage = message;
+  return {
+    modCode: (code, newCode) => {
+      accessCode = newCode;
+      return accessCode;
+    },
+    modMessage: (code, newMessage) => {
+      secretMessage = newMessage;
+      return secretMessage
+    },
+    revealMessage: (newCode) => {
+      if (newCode === accessCode) return secretMessage;
+      if (accessCode !== newCode) throw 'invalid code';
+    }
   }
-};
-lockBox.prototype.modCode = (code, newCode) => {
-  this.code = newCode;
-  return this.code;
 }
-lockBox.prototype.modMessage = (code, newMessage) => {
-  this.message = newMessage;
-  return this.message
-}
-lockBox.prototype.revealMessage = (newCode2) => {
-  if (newCode2 === this.code) return this.message;
-  if (this.code !== newCode2) throw 'error';
-}
+
 module.exports = { lockBox };
