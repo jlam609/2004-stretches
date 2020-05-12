e977805475cea83cf77f8e7a4b83c82f23c42a11
jlam609 <jlam609@gmail.com>
2020-05-11 21:58:40 -0400

    finished

--- questions/11_romanInteger/romanInteger.js
+++ questions/11_romanInteger/romanInteger.js
@@ -27,20 +27,26 @@ const romanInteger = (num) => {
   if (typeof num !== 'number' || num <= 0 || num > 3999) throw 'error'
   let roman = {
     M:1000,
+    CM:900,
     D:500,
+    CD:400,
     C:100,
+    XC:90,
     L:50,
+    XL:40,
     X:10,
+    IX:9,
     V:5,
+    IV:4,
     I:1
   }
   let str = []
   let objKeys = Object.keys(roman)
   while (num > 0) {
     for (let i = 0; i < objKeys.length; i++){
-      if (roman[objKeys] <= num) {
-        num -= roman[objKeys]
-        str.push(objKeys)
+      if (roman[objKeys[i]] <= num) {
+        num -= roman[objKeys[i]]
+        str.push(objKeys[i])
         i--
   }
   }
