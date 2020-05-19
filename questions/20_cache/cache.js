3c2083fee9ce3fbda290239ee525a1c3f1c281e8
jlam609 <jlam609@gmail.com>
2020-05-19 18:56:00 -0400

    format

--- questions/20_cache/cache.js
+++ questions/20_cache/cache.js
@@ -2,15 +2,15 @@
 
 function cache(func) {
   // YOUR CODE
-if (typeof func !== 'function') throw new Error('Input must be a function.')
-let map = {}
-return invokeOnce = (...num) => {
-  if (num in map) return map[num]
-   else  {
-     map[num] = func(...num)
-     return map[num]
-   }
-  }
+  if (typeof func !== "function") throw new Error("Input must be a function.");
+  let map = {};
+  return (invokeOnce = (...num) => {
+    if (num in map) return map[num];
+    else {
+      map[num] = func(...num);
+      return map[num];
+    }
+  });
 }
 
 module.exports = { cache };
