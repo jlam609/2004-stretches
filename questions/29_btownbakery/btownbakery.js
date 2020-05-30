1fcddbac2e6118bfe27ac35ba16c2a6c1c7a8f83
jlam609 <jlam609@gmail.com>
2020-05-30 09:37:31 -0400

    finished;

--- questions/29_btownbakery/btownbakery.js
+++ questions/29_btownbakery/btownbakery.js
@@ -1,19 +1,18 @@
 // B-Town bakery over-bought the supplies to make cake and needs to know how many they can make all together.
 
-const btownbake = (obj1, obj2) => {
+const btownbake = (recipe, supplies) => {
   //enter code here
-  if (typeof obj1 !== 'object' || typeof obj2 !== 'object') throw new Error('must be object')
+  if (typeof recipe !== 'object' || typeof supplies !== 'object') throw new Error('must be object')
   let cakes = 0
-  for (let keys in obj2){
-    if (!obj1[keys]) return cakes;
-    else{
-      if (obj2[keys] - obj1[keys] < 0) return cakes
-      obj2[keys] = obj2[keys] - obj1[keys]
+  for (let keys in recipe){
+      if (!supplies[keys]) return cakes
+      if (((supplies[keys] - recipe[keys]) < 0)) {
+        return cakes
+      }
+      supplies[keys] = (supplies[keys] - recipe[keys])
     }
-}
-console.log(obj2)
-  cakes++
-  return btownbake(obj1, obj2)
+    cakes++
+  return cakes += btownbake(recipe, supplies)
 };
 
 module.exports = { btownbake };
